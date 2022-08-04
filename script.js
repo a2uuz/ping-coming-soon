654f15d1181fd2bfb9b2bfd6e6fbf82d317d4fab
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-08-04 23:32:10 +0300

    update

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const form = document.querySelector("form");
 const input = document.getElementById("Email");
 const btn = document.getElementById("Btn");
 const result = document.getElementById("result");
-form.addEventListener("click", (e) => {
-  e.preventDefault();
+btn.addEventListener("click", (e) => {
 
   if (!input.value) {
     input.style.border = "solid 1px #ff5263";
     result.style.display = "block";
   } else {
     input.style.border = "solid 1px #c2d3ff";
-      result.style.display = "none";
-      input.value = "";
+    result.style.display = "none";
+    input.value = "";
   }
 });
